3453429c2d4369b076a8a1cc408b1fac0deed00d
Rafael Almeida <pentateu@gmail.com>
2015-07-23 14:32:13 +1200

    Initial Commit

---
+++ SpeedTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeedTracker
+//
+//  Created by Rafael Almeida on 23/07/15.
+//  Copyright (c) 2015 ISWE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
